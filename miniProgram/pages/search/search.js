b94f7ab2d9183155e5d527fdb6f61a903714d78c
wb_wangxiaoguang02 <wb_wangxiaoguang02@meituan.com>
2024-05-16 18:47:48 +0800

    构建搜索条件页面

--- miniProgram/pages/search/search.js
+++ miniProgram/pages/search/search.js
@@ -21,6 +21,97 @@ const options = [
   }
 ]
 
+const subwayDetail = [
+  {
+    text: '12号线',
+    badge: 3,
+    dot: false,
+    disabled: false,
+    children: [
+      {
+        text: '东陆路',
+        id: 1,
+        disabled: false
+      },
+      {
+        text: '巨峰路',
+        id: 2
+      },
+      {
+        text: '杨高北路',
+        id: 3
+      }
+    ]
+  },
+  {
+    text: '6号线',
+    badge: 3,
+    dot: false,
+    disabled: false,
+    children: [
+      {
+        text: '东靖路',
+        id: 10,
+        disabled: false
+      },
+      {
+        text: '五洲大道',
+        id: 11
+      },
+      {
+        text: '巨峰路',
+        id: 12
+      }
+    ]
+  },
+  {
+    text: '9号线',
+    badge: 3,
+    dot: false,
+    disabled: false,
+    children: [
+      {
+        text: '台儿庄路',
+        id: 20,
+        disabled: false
+      },
+      {
+        text: '金桥',
+        id: 21
+      },
+      {
+        text: '蓝天路',
+        id: 22
+      }
+    ]
+  },
+  {
+    text: '10号线',
+    badge: 4,
+    dot: false,
+    disabled: false,
+    children: [
+      {
+        text: '双江路',
+        id: 30,
+        disabled: false
+      },
+      {
+        text: '高桥',
+        id: 31
+      },
+      {
+        text: '高桥西',
+        id: 32
+      },
+      {
+        text: '港城',
+        id: 33
+      }
+    ]
+  }
+]
+
 Page({
   data: {
     // 自己的数据
@@ -29,74 +120,47 @@ Page({
     // treeSelect的数据
     mainActiveIndex: 0,
     activeId: null,
-    items: [
-      {
-        text: '所有城市',
-        badge: 3,
-        dot: true,
-        disabled: false,
-        children: [
-          {
-            text: '温州',
-            id: 1,
-            disabled: false
-          },
-          {
-            text: '杭州',
-            id: 2
-          }
-        ]
-      },
-      {
-        text: '租金',
-        badge: 4,
-        dot: true,
-        disabled: false,
-        children: [
-          {
-            text: '1000-2000',
-            id: 3,
-            disabled: false
-          },
-          {
-            text: '2000-3000',
-            id: 4
-          },
-          {
-            text: '3000-4000',
-            id: 5
-          }
-        ]
-      },
-      {
-        text: '户型',
-        badge: 5,
-        dot: true,
-        disabled: false,
-        children: [
-          {
-            text: '一室户',
-            id: 6,
-            disabled: false
-          },
-          {
-            text: '单间',
-            id: 7
-          },
-          {
-            text: '整租',
-            id: 8
-          }
-        ]
-      }
-    ],
+    subwayDetail,
+    // treeSelect的数据
 
     // castar的数据
     show: false,
     options,
     fieldValue: '',
-    cascaderValue: ''
+    cascaderValue: '',
+    // castar的数据
+
+    // 下拉菜单
+    switchTitle1: '包邮',
+    switchTitle2: '团购',
+    itemTitle: '筛选',
+    itemTitleSubway: '地铁线路',
+    itemTitleRent: '租金范围',
+    option1: [
+      { text: '全部商品', value: 0 },
+      { text: '新款商品', value: 1 },
+      { text: '活动商品', value: 2 }
+    ],
+    value1: 0
+    // 下拉菜单
   },
+
+  // DropdownMenu下拉菜单
+  onConfirm() {
+    this.selectComponent('#item').toggle()
+  },
+
+  onSwitch1Change({ detail }) {
+    this.setData({ switch1: detail })
+  },
+
+  onSwitch2Change({ detail }) {
+    this.setData({ switch2: detail })
+  },
+
+  // DropdownMenu下拉菜单
+
+  // TreeSelect分类选择
   onClickNav({ detail = {} }) {
     this.setData({
       mainActiveIndex: detail.index || 0
@@ -108,16 +172,13 @@ Page({
 
     this.setData({ activeId })
   },
+  // TreeSelect分类选择
 
+  // 搜索的组件
   onSearch(e) {
     console.log('用户搜索   ' + e.detail)
   },
-  // searchEvent(e) {
-  //   console.log('用户搜索' + e.detail)
-  //   setTimeout(() => {
-  //     wx.hideLoading()
-  //   }, 1000)
-  // },
+
   onClick() {
     this.setData({
       show: true
@@ -129,17 +190,7 @@ Page({
       show: false
     })
   },
-
-  onFinish(e) {
-    const { selectedOptions, value } = e.detail
-    const fieldValue = selectedOptions
-      .map((option) => option.text || option.name)
-      .join('/')
-    this.setData({
-      fieldValue,
-      cascaderValue: value
-    })
-  },
+  // 搜索的组件
 
   /**
    * Called when user click on the top right corner to share
